55aea5ba78de3c5c09e3f94808d3ab8a6cf5911e
skystash <fa.repo.dev@gmail.com>
2022-06-02 17:51:53 +0100

    First commit

---
+++ app.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Inventory</h1>
+    <nav>
+      <ul>
+        <li>Items</li>
+        <li>Inventory</li>
+      </ul>
+    </nav>
+    <hr>
+    <Test />
+  </div>
+</template>
