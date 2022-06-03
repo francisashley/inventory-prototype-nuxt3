9387d03ceb81a6724d2ec99b279ed5412249cca0
skystash <fa.repo.dev@gmail.com>
2022-06-03 10:33:06 +0100

    Update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 module.exports = {
-  mode: "jit",
-  purge: [
-    "./components/**/*.{vue,js}",
-    "./layouts/**/*.vue",
-    "./app.vue",
-    "./pages/**/*.vue",
-    "./plugins/**/*.{js,ts}",
-    "./nuxt.config.{js,ts}",
+  mode: 'jit',
+  content: [
+    './components/**/*.{vue,js}',
+    './layouts/**/*.vue',
+    './app.vue',
+    './pages/**/*.vue',
+    './plugins/**/*.{js,ts}',
+    './nuxt.config.{js,ts}',
   ],
   theme: {
     extend: {},
