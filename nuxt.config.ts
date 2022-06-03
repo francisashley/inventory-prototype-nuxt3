c31a6af66bcafc14325f2c1d3000c5096924848e
skystash <fa.repo.dev@gmail.com>
2022-06-03 10:41:59 +0100

    Use tailwind nuxt package

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,15 +3,7 @@ import { defineNuxtConfig } from 'nuxt'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   css: ['~/assets/css/tailwind.css'],
-  build: {
-    postcss: {
-      postcssOptions: {
-        plugins: {
-          tailwindcss: {},
-          autoprefixer: {},
-        },
-      },
-    },
-  },
+  components: ['@/components', '@/components/layout'],
+  modules: ['@nuxtjs/tailwindcss'],
   buildModules: ['@nuxtjs/eslint-module'],
 })
