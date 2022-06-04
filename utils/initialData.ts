1d714fb02363b5a79c72abc555ea46c030376be8
skystash <fa.repo.dev@gmail.com>
2022-06-04 18:38:09 +0100

    Use more random amounts

--- utils/initialData.ts
+++ utils/initialData.ts
@@ -6,8 +6,10 @@ import fakeContainers from '@/assets/fixtures/containers.json'
 const initialItems = fakeItems.map(createItem)
 const initialContainers = fakeContainers.map((container) => createContainer(container))
 
+const randomAmount = [1, 2, 1, 10, 1, 3, 9]
+
 for (const [i, item] of initialItems.entries()) {
-  initialContainers[0] = addItemToContainer(initialContainers[0], item, { amount: i })
+  initialContainers[0] = addItemToContainer(initialContainers[0], item, { amount: randomAmount[i] || 1 })
 }
 
 export { initialItems, initialContainers }
