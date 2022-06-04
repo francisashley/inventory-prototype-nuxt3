8668d053497ff923c3fffd45beff84bb70da79b5
skystash <fa.repo.dev@gmail.com>
2022-06-04 18:25:36 +0100

    Revert

--- app.vue
+++ app.vue
@@ -1,20 +1,28 @@
 <template>
   <div class="min-h-screen flex flex-col flex-1 bg-gray-900">
-    <div v-for="({ id, items, theme }, i) in getContainers" :key="i" class="mx-auto mt-8">
+    <div v-for="({ id, items, theme }, i) in getEnrichedContainers" :key="i" class="mx-auto mt-8">
       <Container :id="id" :items="items" :theme="theme" :rows="2" :cols="8" @change="onChange" />
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { enrichContainerItems } from '@/utils/dataTools'
+import { findItemById } from '@/utils/dataTools'
 import { initialItems, initialContainers } from '@/utils/initialData'
 
 const items = ref(initialItems)
 const containers = ref(initialContainers)
 
-const getContainers = computed(() => {
-  return [...containers.value].map((container) => enrichContainerItems(container, items.value))
+const getEnrichedContainers = computed(() => {
+  return [...containers.value].map((container) => {
+    return {
+      ...container,
+      items: [...container.items].map((item) => ({
+        ...item,
+        ...findItemById(items.value, item.id),
+      })),
+    }
+  })
 })
 
 const onChange = ({ items, id }) => {
