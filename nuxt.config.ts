d38683874b434ee8384827eae1f863b3772aeaac
skystash <fa.repo.dev@gmail.com>
2022-06-05 12:47:01 +0100

    Update nuxt.config.js

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ import { defineNuxtConfig } from 'nuxt'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   css: ['~/assets/css/tailwind.css'],
-  components: ['@/components', '@/components/layout'],
+  components: ['@/components'],
   modules: ['@nuxtjs/tailwindcss'],
   buildModules: ['@nuxtjs/eslint-module'],
 })
