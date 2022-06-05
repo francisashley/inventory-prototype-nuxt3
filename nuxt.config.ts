5914d002145ade6c84f916df84056bb3ca914bc2
skystash <fa.repo.dev@gmail.com>
2022-06-05 12:49:53 +0100

    Make client side

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  ssr: false,
   css: ['~/assets/css/tailwind.css'],
   components: ['@/components'],
   modules: ['@nuxtjs/tailwindcss'],
