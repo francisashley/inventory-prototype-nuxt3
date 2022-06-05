810c77849285aaedafd6dca78cec27a1065ad9a1
skystash <fa.repo.dev@gmail.com>
2022-06-05 15:52:18 +0100

    Randomise initial amount

--- utils/initialData.ts
+++ utils/initialData.ts
@@ -1,16 +1,14 @@
 import toolset from '@/utils/toolset'
 
-import fakeItems from '@/assets/fixtures/item-catalogue.json'
+import itemCatalogue from '@/assets/fixtures/item-catalogue.json'
 import fakeContainers from '@/assets/fixtures/containers.json'
 
-const initialItems = fakeItems.map(toolset.createItem)
+const initialItems = itemCatalogue.map(toolset.createItem)
 const initialContainers = fakeContainers.map((container) => toolset.createContainer(container as looseContainer))
 
-const amounts = [1, 2, 1, 10, 1, 3, 9]
-
-for (const [i, item] of initialItems.entries()) {
-  initialContainers[0] = toolset.container(initialContainers[0]).appendItem({ ...item, amount: amounts[i] || 1 })
-  initialContainers[0] = toolset.container(initialContainers[0]).appendItem({ ...item, amount: amounts[i] || 1 })
+for (const item of initialItems) {
+  const amount = Math.floor(Math.random() * 10)
+  initialContainers[0] = toolset.container(initialContainers[0]).appendItem({ ...item, amount })
 }
 
 export { initialItems, initialContainers }
