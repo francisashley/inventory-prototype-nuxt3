d744f804960b9cb97468952db5bb46a3426a608f
Francis Ashley <francisashley.dev@gmail.com>
2022-12-27 15:14:33 +0000

    refactor: remove unused name

--- composables/useDemo.ts
+++ composables/useDemo.ts
@@ -9,7 +9,6 @@ import { depositFirstAvailableSlot } from '@/utils/container.utils'
 
 type DemoContainer = {
   id: number
-  name: string
   color: string
   slots: ContainerSlot[]
   size: ContainerSize
@@ -22,21 +21,19 @@ const state = reactive<{ itemRegistry: Item[]; demoContainers: DemoContainer[] }
 
 state.itemRegistry = itemFixtures.map(createItem)
 
-state.demoContainers = containerFixtures.map(
-  ({ name, color, size }: { name: string; color: string; size: ContainerSize }, i) => {
-    const id = generateId()
-    let slots = generateSlots(id, size, [])
+state.demoContainers = containerFixtures.map(({ color, size }: { color: string; size: ContainerSize }, i) => {
+  const id = generateId()
+  let slots = generateSlots(id, size, [])
 
-    if (i === 0) {
-      for (let i = 0; i < 12; i++) {
-        const item = getRandomItem(state.itemRegistry)
-        slots = depositFirstAvailableSlot(slots, item)
-      }
+  if (i === 0) {
+    for (let i = 0; i < 12; i++) {
+      const item = getRandomItem(state.itemRegistry)
+      slots = depositFirstAvailableSlot(slots, item)
     }
-
-    return { id, name, color, slots, size }
   }
-)
+
+  return { id, color, slots, size }
+})
 
 export function useDemo() {
   const demoContainers = computed(() => state.demoContainers)
